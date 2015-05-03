7633bc45ae37a499aea6657ba3fafe5f31fa1525
John Corley <john@corley.co>
2015-05-03 08:46:25 +0100

    rainy Sunday

--- app/views/layout-account.blade.php
+++ app/views/layout-account.blade.php
@@ -4,7 +4,6 @@
         <!-- Bootstrap CSS & JS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
-        <script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
         {{ HTML::style( asset('css/admin.css') ) }}
         @yield('stylesheets')
@@ -75,7 +74,7 @@
             @yield('content')
             
             @yield('footer')
-            
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         </div>
     </body>
 </html>
