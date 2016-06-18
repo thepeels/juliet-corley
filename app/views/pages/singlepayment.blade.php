bef39f3e2e9e4040bbb1b4709e3905f9c5238c09
John Corley <john@corley.co>
2016-06-18 06:42:18 +0100

    zipcode saved on stripe dashboard

--- app/views/pages/singlepayment.blade.php
+++ app/views/pages/singlepayment.blade.php
@@ -27,10 +27,10 @@
 {{Form::label('text','Please send my receipt e-mail to:')}}
 {{Form::input('email','receipt_email',null,['class'=>'newclass aligned-right','placeholder'=>'e-mail','size'=>'35'])}}<br>
 {{$errors->first('receipt_email','<small class="red-error">:message</small>')}}
-</br>
+{{--</br>
 {{Form::label('text','CardHolder Name (as it appears on the card):')}}
 {{Form::input('text','cardholder_name',null,['class'=>'newclass aligned-right','placeholder'=>'name','size'=>'35'])}}<br>
-{{$errors->first('cardholder_name','<small class="red-error">:message</small>')}}
+{{$errors->first('cardholder_name','<small class="red-error">:message</small>')}}--}}
 </br>
 <span class="payment-errors"></span>
 {{Form::label('text','Description of item agreed for purchase')}}
--- app/views/pages/striper.blade.php
+++ app/views/pages/striper.blade.php
@@ -14,23 +14,22 @@ $receipt_email = Input::get('receipt_email');
 ?>
 <div class="cart merri">
     <h3>Pay <span class= "julie">Juliet Corley</span> $<?=$amountindollars?> for <?=$itemdescription?></h3>
-    <form action="{{url('payment/singlepayment')}}" method="POST">
-        <input name ="amountincents" type="hidden" value="<?=$amountincents;?>">
-        <input name ="itemdescription" type="hidden" value="<?=$itemdescription;?>">
-        <input name ="cardholder_name" type="hidden" value="<?=$cardholder_name;?>">
-        <input name ="receipt_email" type="hidden" value="<?=$receipt_email;?>">
+        <form action="{{url('payment/singlepayment')}}" method="POST">
+            <input name ="amountincents" type="hidden" value="<?=$amountincents;?>">
+            <input name ="itemdescription" type="hidden" value="<?=$itemdescription;?>">
+            <input name ="receipt_email" type="hidden" value="<?=$receipt_email;?>">
 
-        <script
-            src="https://checkout.stripe.com/v2/checkout.js" class="stripe-button waiting" style="display:none"
-            data-key="{{Config::get($_ENV['STRIPE_CONFIG'])}}"//from .env.local.php or .env.php
-            data-amount="<?=$amountincents;?>"
-            data-metadata={'entered-card-name':'<?$cardholder_name?>'}
-            data-name="JulietCorley.com"
-            data-description="<?=$itemdescription;?>"
-            data-receipt_email="<?=$receipt_email;?>"
-            data-zip-code="true"
-            data-image="">
-        </script>
-     </form>
+            <script
+                src="https://checkout.stripe.com/checkout.js" class="stripe-button waiting"
+                data-key="{{Config::get($_ENV['STRIPE_CONFIG'])}}"
+                data-amount="<?=$amountincents;?>"
+                data-metadata={'entered-card-name':'<?$cardholder_name?>'}
+                data-name="JulietCorley.com"
+                data-description="<?=$itemdescription;?>"
+                data-receipt_email="<?=$receipt_email;?>"
+                data-zip-code="true"
+                data-image="">
+            </script>
+        </form>
 </div>
 @stop
