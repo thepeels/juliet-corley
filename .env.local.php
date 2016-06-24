66caf46403cba745042a193cbccdc913ab22de63
John Corley <john@corley.co>
2016-06-24 16:15:22 +0100

    .env removed

--- .env.local.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-
-    'STRIPE_KEY'            => 'sk_test_bpS6MMbSZ8yytf5gM42M2I3i',
-    'STRIPE_PUBLIC_KEY'     => 'pk_test_LPxLxeit0MG4COw22QB7bW6q',
-    'STRIPE_CONFIG'         => 'stripetest.stripe.public',
-
-);
