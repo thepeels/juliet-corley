ea415a9da9f76c4c4b3f4b1604328105b1cd1770
John Corley <john@corley.co>
2019-01-01 21:36:30 +0000

    error logic reversed

--- app/filters.php
+++ app/filters.php
@@ -116,7 +116,7 @@ Route::filter('csrf', function()
 App::error(function(Exception $exception,$code)
 {
     Log::error($exception);
-    if ($_ENV['DEBUG'] == true){
+    if ($_ENV['DEBUG'] == false){
         //dd('hi');
         return Redirect::to('error')->with([
             'title' => 'error'
